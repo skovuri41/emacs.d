1a9db3578c5a43ac7a31ae38ae6897cd9b865afe
surya aditya <surya46584@gmail.com>
2015-08-28 15:29:11 -0400

    change my emac and init files to load on mac

--- init.el
+++ init.el
@@ -51,39 +51,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
- '(custom-safe-themes
-   (quote
-    ("9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" default)))
- '(fci-rule-color "#383838")
- '(org-bullets-bullet-list (quote ("●" "○" "►" "✿")))
  '(package-selected-packages
    (quote
-    (org-plus-contrib zoom-frm zenburn-theme workgroups2 workgroups use-package undo-tree smex smartparens smart-mode-line rainbow-delimiters plantuml-mode page-break-lines org-journal org-bullets org multiple-cursors magit latest-clojure-libraries jump-char json-mode ipretty inf-mongo iedit ido-vertical-mode htmlize helm-swoop helm-projectile helm-orgcard helm-gtags helm-descbinds guide-key grizzl gitignore-mode gitconfig-mode git-messenger git-gutter-fringe git-blame flx-ido expand-region drag-stuff direx deft company clojure-snippets clojure-cheatsheet buffer-move bookmark+ ace-window ace-jump-buffer ac-cider 4clojure)))
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
+    (git-commit-mode zoom-frm zenburn-theme workgroups2 workgroups use-package undo-tree smex smartparens smart-mode-line rainbow-delimiters plantuml-mode page-break-lines org-plus-contrib org-journal org-bullets org multiple-cursors magit latest-clojure-libraries jump-char json-mode ipretty inf-mongo iedit ido-vertical-mode htmlize helm-swoop helm-projectile helm-orgcard helm-gtags helm-descbinds guide-key grizzl gitignore-mode gitconfig-mode git-messenger git-gutter-fringe git-blame flx-ido expand-region drag-stuff direx deft company clojure-snippets clojure-cheatsheet buffer-move bookmark+ ace-window ace-jump-buffer ac-cider 4clojure)))
+
+'(org-bullets-bullet-list (quote ("◉" "○" "►" "✿")))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
