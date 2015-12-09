7622816f0fc598c3578d14d18d8927480c6f24d0
shyam sunder k <surya46584@gmail.com>
2015-12-08 22:49:25 -0500

    Cask version

--- Cask
+++ Cask
@@ -1,45 +1,76 @@
 (source gnu)
 (source melpa)
+(source marmalade)
+(source org)
 
 (depends-on "cask")
+(depends-on "use-package")
+(depends-on "better-defaults")
 (depends-on "dash")
-(depends-on "drag-stuff")
-(depends-on "expand-region")
+
+;git
 (depends-on "magit")
-(depends-on "multiple-cursors")
+
+
 (depends-on "popwin")
 (depends-on "projectile")
-(depends-on "s")
-(depends-on "smex")
-(depends-on "wrap-region")
+(depends-on "grizzl")
+
 (depends-on "yasnippet")
-(depends-on "clojure-snippets")
+
 (depends-on "yaml-mode")
 (depends-on "markdown-mode")
-(depends-on "glsl-mode")
-(depends-on "evil")
-(depends-on "evil-leader")
-(depends-on "evil-cleverparens")
-(depends-on "evil-surround")
-(depends-on "evil-org")
+(depends-on "json-mode")
+
 (depends-on "powerline")
 (depends-on "diminish")
-(depends-on "paredit")
+(depends-on "company")
+
+;;org
+(depends-on "org")
+(depends-on "org-bullets")
+
+;;clojure
 (depends-on "smartparens")
 (depends-on "clojure-mode")
-(depends-on "typed-clojure-mode")
+(depends-on "clojure-snippets")
 (depends-on "paren-face")
 (depends-on "cider")
-(depends-on "slamhound")
 (depends-on "clj-refactor")
-(depends-on "company")
-(depends-on "use-package")
-(depends-on "ag")
+(depends-on "rainbow-mode")
+(depends-on "clojure-cheatsheet")
+
+
+
 (depends-on "guide-key")
 (depends-on "flx-ido")
 (depends-on "ido-vertical-mode")
-(depends-on "better-defaults")
 (depends-on "fancy-narrow")
 (depends-on "aggressive-indent")
-(depends-on "rainbow-mode")
+(depends-on "smex")
+
+(depends-on "ace-jump-buffer")
+(depends-on "ace-window")
+(depends-on "buffer-move")
+(depends-on "undo-tree")
+(depends-on "ace-jump-mode")
+(depends-on "drag-stuff")
+(depends-on "expand-region")
+(depends-on "multiple-cursors")
+(depends-on "wrap-region")
+(depends-on "move-text")
+
+
+;helm
+(depends-on "helm")
+(depends-on "helm-swoop")
+(depends-on "helm-google")
+(depends-on "helm-projectile")
+(depends-on "helm-gtags")
+(depends-on "helm-descbinds")
+
+
+
+
+
 
--- init.el
+++ init.el
@@ -1,63 +1,34 @@
-(require 'package)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+;;;; Initialize ;;;;
 
-(package-initialize)
+(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
+(cask-initialize)
 
-;; This sets up the load path so that we can override it
-(package-initialize nil)
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(require 'use-package)
 
-;; Override the packages with the git version of Org and other packages
-(add-to-list 'load-path "~/.emacs.d/elisp/org-mode/lisp")
-(setq org-ditaa-jar-path "~/.emacs.d/elisp/org-mode/contrib/scripts/ditaa.jar")
+;; Add /usr/local/bin to path
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
 
-;; Load the rest of the packages
-(package-initialize t)
-(setq package-enable-at-startup nil)
-(defface org-block-background
-       '((t (:background "#474747")))
-       "Face used for the source block background.")
+(setq default-directory (getenv "HOME"))
 
-(require 'org)
-;;(require 'org-plus-contrib)
-(require 'ob)
-(require 'ob-tangle)
+(server-start)
 
-(require 'ob-clojure)
-(require 'cl)
-(require 'ob-ditaa)
+(use-package better-defaults)
 
-(setq my-emacs-directory (file-name-directory (file-truename load-file-name)))
-(org-babel-load-file (expand-file-name "my-emacs.org" my-emacs-directory))
+(setq inhibit-startup-message t)
+(blink-cursor-mode 0)
+(menu-bar-mode 1)
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((sh         . t)
-   (js         . t)
-   (emacs-lisp . t)
-   (perl       . t)
-   (scala      . t)
-   (clojure    . t)
-   (python     . t)
-   (dot        . t)
-   (css        . t)
-   (plantuml   . t)
-   (ditaa . t)))
+;; Disable annoying visible bell on OSX
+(setq visible-bell nil)
 
-(setq org-confirm-babel-evaluate nil)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-
-'(org-bullets-bullet-list (quote ("◉" "○" "►" "✿")))
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Actually, why not disable the annoying audible bell as well
+(setq ring-bell-function 'ignore)
 
 
+;; Buffer settings
+(setq default-indicate-empty-lines t)
+(setq require-final-newline t)
+(setq show-trailing-whitespace t)
