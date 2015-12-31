5c1b5930b1123eb05b5e933694f6abb229a921eb
shyam sunder k <surya46584@gmail.com>
2015-12-30 23:42:38 -0500

    expand region

--- elisp/init-expand-region.el
+++ elisp/init-expand-region.el
@@ -1,3 +1,13 @@
 (use-package expand-region
-  :bind ("C-=" . er/expand-region))
+  :ensure t
+  :defer t
+  :after (evil)
+  :config
+    (define-key evil-normal-state-map (kbd "+") 'er/expand-region)
+    (define-key evil-visual-state-map (kbd "x") 'er/expand-region)
+    (define-key evil-insert-state-map (kbd "M-=") 'er/expand-region)
+    (define-key evil-visual-state-map (kbd "X") 'er/contract-region)
+    (define-key evil-visual-state-map (kbd "+") 'er/expand-region)
+    (define-key evil-visual-state-map (kbd "-") 'er/contract-region)
+    (setq expand-region-contract-fast-key "z"))
 (provide 'init-expand-region)
