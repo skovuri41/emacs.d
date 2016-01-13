0df8e18c5224de363a3c07757759d5b25b930cdc
shyam sunder k <surya46584@gmail.com>
2016-01-13 14:28:14 -0500

    add init-defuns file to branch

---
+++ elisp/init-defuns.el
@@ -0,0 +1,383 @@
+;; masteringemacs.org fixing mark commands tmm
+(defun push-mark-no-activate ()
+  "Pushes `point' to `mark-ring' and does not activate the region
+   Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
+  (interactive)
+  (push-mark (point) t nil)
+  (message "Pushed mark to ring"))
+
+(defun jump-to-mark ()
+  "Jumps to the local mark, respecting the `mark-ring' order.
+  This is the same as using \\[set-mark-command] with the prefix argument."
+  (interactive)
+  (set-mark-command 1))
+
+(defun exchange-point-and-mark-no-activate ()
+  "Identical to \\[exchange-point-and-mark] but will not activate the region."
+  (interactive)
+  (exchange-point-and-mark)
+  (deactivate-mark nil))
+
+(defun save-macro (name)                  
+   "save a macro. Take a name as argument
+    and save the last defined macro under 
+    this name at the end of your .emacs"
+    (interactive "SName of the macro :")  ; ask for the name of the macro    
+    (kmacro-name-last-macro name)         ; use this name for the macro    
+    (find-file user-init-file)            ; open ~/.emacs or other user init file 
+    (goto-char (point-max))               ; go to the end of the .emacs
+    (newline)                             ; insert a newline
+    (insert-kbd-macro name)               ; copy the macro 
+    (newline)                             ; insert a newline
+    (switch-to-buffer nil))               ; return to the initial buffer
+
+(defun reindent-whole-buffer ()
+  "Reindent the whole buffer."
+  (interactive)
+  (indent-region (point-min)
+                 (point-max)))
+
+;; To facilitate easier keyboard macro usage (from http://www.emacswiki.org/emacs/KeyboardMacros)
+(defun toggle-kbd-macro-recording-on ()
+  "One-key keyboard macros: turn recording on."
+  (interactive)
+  (define-key global-map (this-command-keys)
+    'toggle-kbd-macro-recording-off)
+  (start-kbd-macro nil))
+
+(defun toggle-kbd-macro-recording-off ()
+  "One-key keyboard macros: turn recording off."
+  (interactive)
+  (define-key global-map (this-command-keys)
+    'toggle-kbd-macro-recording-on)
+  (end-kbd-macro))
+
+
+;;; Open Lines
+
+(defun open-line-below ()
+  (interactive)
+  (end-of-line)
+  (newline)
+  (indent-for-tab-command))
+
+(defun open-line-above ()
+  (interactive)
+  (beginning-of-line)
+  (newline)
+  (forward-line -1)
+  (indent-for-tab-command))
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+
+;;;;Join Lines
+(defun join-lines ()
+   "If at the end of the line, will join the following line to the
+   end of this one...unless it is blank, in which case, it will
+   keep joining lines until the next line with text is
+   connected."
+   (interactive)
+   ;; Move to the the beginning of the white space before attempting
+   ;; this process. This allows us to join lines even if we are in the
+   ;; middle of some empty lines.
+   (re-search-backward "[^[:space:]\\r\\n]")
+   (forward-char)
+   ;; Just in case we have some trailing whitespace we can't see, let's
+   ;; just get rid of it. Won't do anything if in the middle of a line,
+   ;; or if there is not trailing whitespace.
+   (delete-trailing-whitespace (point) (point-at-eol))
+   ;; While we are at the end of the line, join a line, remove the
+   ;; whitespace, and keep on going until we're through...
+   (while (eq (point-at-eol) (point))
+     (delete-char 1)
+     (delete-trailing-whitespace (point) (point-at-eol))))
+
+;;;; copy-line with variable arugments
+(defun copy-line (&optional arg)
+  "Copy lines (as many as prefix argument) in the kill ring"
+  (interactive "p")
+  (kill-ring-save (line-beginning-position)
+                  (line-beginning-position (+ 1 arg)))
+  (message "%d line(s) copied" arg ))
+
+
+(defun nxml-pretty-print-buffer ()
+  "pretty print the XML in a buffer."
+  (interactive)
+  (nxml-pretty-print-region (point-min) (point-max)))
+
+;; XML pretty print
+(defun pretty-print-xml-region (begin end)
+  (interactive "r")
+  (save-excursion
+    (nxml-mode)
+    (goto-char begin)
+    (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+      (backward-char) (insert "\n"))
+    (indent-region begin end))
+  (message "Ah, much better!"))
+
+(defun nxml-kill-tag-contents ()
+  "Copy the contents between two tags"
+;  (interactive "*p\ncCopy tag contents: ") ; this expects arguments input
+  (interactive)
+  (nxml-backward-up-element)
+  (kill-region
+    (progn (search-forward ">")
+      (point))
+    (progn (nxml-backward-up-element)
+      (nxml-forward-element)
+      (search-backward "</")
+      (point))))
+
+(defun nxml-copy-tag-contents ()
+  "Copy the contents between two tags"
+;  (interactive "*p\ncCopy tag contents: ") ; this expects arguments input
+  (interactive)
+  (nxml-backward-up-element)
+  (copy-region-as-kill
+   (progn (search-forward ">") (point))
+   (progn (nxml-backward-up-element)
+     (nxml-forward-element)
+     (search-backward "</")
+     (point))))
+
+
+(defun xah-cut-line-or-region ()
+  "Cut current line, or text selection.
+When `universal-argument' is called first, cut whole buffer (respects `narrow-to-region').
+
+URL `http://ergoemacs.org/emacs/emacs_copy_cut_current_line.html'
+Version 2015-06-10"
+  (interactive)
+  (if current-prefix-arg
+      (progn ; not using kill-region because we don't want to include previous kill
+        (kill-new (buffer-string))
+        (delete-region (point-min) (point-max)))
+    (progn (if (use-region-p)
+               (kill-region (region-beginning) (region-end) t)
+             (kill-region (line-beginning-position) (line-beginning-position 2))))))
+
+(defun xah-copy-line-or-region ()
+  "Copy current line, or text selection.
+When called repeatedly, append copy subsequent lines.
+When `universal-argument' is called first, copy whole buffer (respects `narrow-to-region').
+
+URL `http://ergoemacs.org/emacs/emacs_copy_cut_current_line.html'
+Version 2015-09-18"
+  (interactive)
+  (let (ξp1 ξp2)
+    (if current-prefix-arg
+        (progn (setq ξp1 (point-min))
+               (setq ξp2 (point-max)))
+      (progn
+        (if (use-region-p)
+            (progn (setq ξp1 (region-beginning))
+                   (setq ξp2 (region-end)))
+          (progn (setq ξp1 (line-beginning-position))
+                 (setq ξp2 (line-end-position))))))
+    (if (eq last-command this-command)
+        (progn
+          (kill-append "\n" nil)
+          (forward-line 1)
+          (end-of-line)
+          (kill-append (buffer-substring-no-properties (line-beginning-position) (line-end-position)) nil)
+          (message "Line copy appended"))
+      (progn
+        (kill-ring-save ξp1 ξp2)
+        (if current-prefix-arg
+            (message "Buffer text copied")
+          (message "Text copied"))))))
+
+(defun xah-copy-to-register-1 ()
+  "Copy current line or text selection to register 1.
+See also: `xah-paste-from-register-1', `copy-to-register'."
+  (interactive)
+  (let (p1 p2)
+    (if (region-active-p)
+        (progn (setq p1 (region-beginning))
+               (setq p2 (region-end)))
+      (progn (setq p1 (line-beginning-position))
+             (setq p2 (line-end-position))))
+    (copy-to-register ?1 p1 p2)
+    (message "copied to register 1: %s." (buffer-substring-no-properties p1 p2))))
+
+(defun xah-paste-from-register-1 ()
+  "Paste text from register 1.
+See also: `xah-copy-to-register-1', `insert-register'."
+  (interactive)
+  (when (use-region-p)
+    (delete-region (region-beginning) (region-end) )
+    )
+  (insert-register ?1 t))
+
+
+
+(defvar xah-switch-buffer-ignore-dired t "If t, ignore dired buffer when calling `xah-next-user-buffer' or `xah-previous-user-buffer'")
+(setq xah-switch-buffer-ignore-dired t)
+
+(defun xah-next-user-buffer ()
+  "Switch to the next user buffer.
+ “user buffer” is a buffer whose name does not start with “*”.
+If `xah-switch-buffer-ignore-dired' is true, also skip directory buffer.
+2015-01-05 URL `http://ergoemacs.org/emacs/elisp_next_prev_user_buffer.html'"
+  (interactive)
+  (next-buffer)
+  (let ((i 0))
+    (while (< i 20)
+      (if (or
+           (string-equal "*" (substring (buffer-name) 0 1))
+           (if (string-equal major-mode "dired-mode")
+               xah-switch-buffer-ignore-dired
+             nil
+             ))
+          (progn (next-buffer)
+                 (setq i (1+ i)))
+        (progn (setq i 100))))))
+
+(defun xah-previous-user-buffer ()
+  "Switch to the previous user buffer.
+ “user buffer” is a buffer whose name does not start with “*”.
+If `xah-switch-buffer-ignore-dired' is true, also skip directory buffer.
+2015-01-05 URL `http://ergoemacs.org/emacs/elisp_next_prev_user_buffer.html'"
+  (interactive)
+  (previous-buffer)
+  (let ((i 0))
+    (while (< i 20)
+      (if (or
+           (string-equal "*" (substring (buffer-name) 0 1))
+           (if (string-equal major-mode "dired-mode")
+               xah-switch-buffer-ignore-dired
+             nil
+             ))
+          (progn (previous-buffer)
+                 (setq i (1+ i)))
+        (progn (setq i 100))))))
+
+(defun xah-next-emacs-buffer ()
+  "Switch to the next emacs buffer.
+ (buffer name that starts with “*”)"
+  (interactive)
+  (next-buffer)
+  (let ((i 0))
+    (while (and (not (string-equal "*" (substring (buffer-name) 0 1))) (< i 20))
+      (setq i (1+ i)) (next-buffer))))
+
+(defun xah-previous-emacs-buffer ()
+  "Switch to the previous emacs buffer.
+ (buffer name that starts with “*”)"
+  (interactive)
+  (previous-buffer)
+  (let ((i 0))
+    (while (and (not (string-equal "*" (substring (buffer-name) 0 1))) (< i 20))
+      (setq i (1+ i)) (previous-buffer))))
+
+
+(defvar xah-left-brackets nil "List of open bracket chars.")
+(setq xah-left-brackets '("(" "{" "[" "<" "〔" "【" "〖" "〈" "《" "「" "『" "“" "‘" "‹" "«" "（"))
+
+(defvar xah-right-brackets nil "list of close bracket chars.")
+(setq xah-right-brackets '(")" "]" "}" ">" "〕" "】" "〗" "〉" "》" "」" "』" "”" "’" "›" "»" "）"))
+
+(defun xah-backward-left-bracket ()
+  "Move cursor to the previous occurrence of left bracket.
+The list of brackets to jump to is defined by `xah-left-brackets'.
+URL `http://ergoemacs.org/emacs/emacs_navigating_keys_for_brackets.html'
+Version 2015-03-24"
+  (interactive)
+  (search-backward-regexp (eval-when-compile (regexp-opt xah-left-brackets)) nil t))
+
+(defun xah-forward-right-bracket ()
+  "Move cursor to the next occurrence of right bracket.
+The list of brackets to jump to is defined by `xah-right-brackets'.
+URL `http://ergoemacs.org/emacs/emacs_navigating_keys_for_brackets.html'
+Version 2015-03-24"
+  (interactive)
+  (search-forward-regexp (eval-when-compile (regexp-opt xah-right-brackets)) nil t))
+
+(defun xah-forward-block (&optional φn)
+  "Move cursor forward to the beginning of next text block.
+A text block is separated by blank lines.
+In most major modes, this is similar to `forward-paragraph', but this command's behavior is the same regardless of syntax table."
+  (interactive "p")
+  (let ((φn (if (null φn) 1 φn)))
+    (search-forward-regexp "\n[\t\n ]*\n+" nil "NOERROR" φn)))
+
+(defun xah-backward-block (&optional φn)
+  "Move cursor backward to previous text block.
+See: `xah-forward-block'"
+  (interactive "p")
+  (let ((φn (if (null φn) 1 φn))
+        (ξi 1))
+    (while (<= ξi φn)
+      (if (search-backward-regexp "\n[\t\n ]*\n+" nil "NOERROR")
+          (progn (skip-chars-backward "\n\t "))
+        (progn (goto-char (point-min))
+               (setq ξi φn)))
+      (setq ξi (1+ ξi)))))
+
+
+(defun xah-make-backup ()
+  "Make a backup copy of current file or dired marked files.
+If in dired, backup current file or marked files.
+The backup file name is
+ ‹name›~‹timestamp›~
+example:
+ file.html~20150721T014457~
+in the same dir. If such a file already exist, it's overwritten.
+If the current buffer is not associated with a file, nothing's done.
+URL `http://ergoemacs.org/emacs/elisp_make-backup.html'
+Version 2015-08-17"
+  (interactive)
+  (let (
+        (ξfile-list
+         (if (string-equal major-mode "dired-mode")
+             (dired-get-marked-files)
+           (list (buffer-file-name)))))
+    (mapc (lambda (ξfname)
+            (let ((ξbackup-name
+                   (concat ξfname "~" (format-time-string "%Y%m%dT%H%M%S") "~")))
+              (copy-file ξfname ξbackup-name t)
+              (message (concat "Backup saved at: " ξbackup-name))))
+          ξfile-list)))
+xah search word at cursor
+
+(defun xah-search-current-word ()
+  "Call `isearch' on current word or text selection.
+“word” here is A to Z, a to z, and hyphen 「-」 and underline 「_」, independent of syntax table.
+URL `http://ergoemacs.org/emacs/modernization_isearch.html'
+Version 2015-04-09"
+  (interactive)
+  (let ( ξp1 ξp2 )
+    (if (use-region-p)
+        (progn
+          (setq ξp1 (region-beginning))
+          (setq ξp2 (region-end)))
+      (save-excursion
+        (skip-chars-backward "-_A-Za-z0-9")
+        (setq ξp1 (point))
+        (right-char)
+        (skip-chars-forward "-_A-Za-z0-9")
+        (setq ξp2 (point))))
+    (setq mark-active nil)
+    (when (< ξp1 (point))
+      (goto-char ξp1))
+    (isearch-mode t)
+    (isearch-yank-string (buffer-substring-no-properties ξp1 ξp2))))
+
+(defun xah-new-empty-buffer ()
+  "Open a new empty buffer.
+URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+Version 2015-06-12"
+  (interactive)
+  (let ((ξbuf (generate-new-buffer "untitled")))
+    (switch-to-buffer ξbuf)
+    (funcall (and initial-major-mode))
+    (setq buffer-offer-save t)))
