fe7f6fcda31ac46b708414676ceb34485ba5ee6c
shyam sunder k <surya46584@gmail.com>
2016-01-29 23:46:23 -0500

    update evil configureation

--- elisp/init-evil.el
+++ elisp/init-evil.el
@@ -61,14 +61,15 @@
     (defface my-evil-state-emacs-face
       '((t (:background "Orange" :foreground "White")))
       "Evil Mode Emacs State Face")
-    
     (defface my-evil-state-insert-face
       '((t (:background "DodgerBlue1" :foreground "White")))
       "Evil Mode Insert State Face")
-
     (defface my-evil-state-normal-face
       '((t (:background "Red" :foreground "White")))
       "Evil Mode Normal Stace Face")
+    (defface my-evil-state-visual-face
+      '((t (:background "Green" :foreground "White")))
+      "Evil Mode Visual Stace Face")
 
     (defun evil-generate-mode-line-tag (&optional state)
       "Generate the evil mode-line tag for STATE."
@@ -81,6 +82,8 @@
                                 'my-evil-state-normal-face)
                                ((string= "insert" state)
                                 'my-evil-state-insert-face)
+                               ((string= "visual" state)
+                                'my-evil-state-visual-face)
                                ((string= "emacs" state)
                                 'my-evil-state-emacs-face))
                         'help-echo (evil-state-property state :name)
