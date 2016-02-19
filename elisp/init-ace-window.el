9dd38e593d55ea1901e483c5934e3773657bf511
shyam sunder k <surya46584@gmail.com>
2016-02-19 13:28:14 -0500

    update ace-window

--- elisp/init-ace-window.el
+++ elisp/init-ace-window.el
@@ -2,6 +2,9 @@
   :ensure t
   :bind ("M-p" . ace-window)
   :config
+  (setq aw-ignore-on t)
+  (add-to-list 'aw-ignored-buffers " *NeoTree*")
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
 
 (provide 'init-ace-window)
+
