bfcabeba7d4341ff3103cb7df6d75b8c42b6093c
shyam sunder k <surya46584@gmail.com>
2016-03-04 16:20:06 -0500

    git gutter hydra

--- elisp/init-git.el
+++ elisp/init-git.el
@@ -9,6 +9,7 @@
 
 (use-package git-gutter+
   :commands global-git-gutter+-mode
+  :diminish ""
   :init
   (progn
     (setq git-gutter+-hide-gutter t)
@@ -23,30 +24,51 @@
         (require 'git-gutter-fringe+)))
     (global-git-gutter+-mode)
     ;; (setq git-gutter-fr+-side 'right-fringe))
-    :config
-    (progn
-      ;; custom graphics that works nice with half-width fringes
-      (fringe-helper-define 'git-gutter-fr+-added nil
-        "..X...."
-        "..X...."
-        "XXXXX.."
-        "..X...."
-        "..X...."
-        )
-      (fringe-helper-define 'git-gutter-fr+-deleted nil
-        "......."
-        "......."
-        "XXXXX.."
-        "......."
-        "......."
-        )
-      (fringe-helper-define 'git-gutter-fr+-modified nil
-        "..X...."
-        ".XXX..."
-        "XX.XX.."
-        ".XXX..."
-        "..X...."
-        ))))
+    )
+  :config
+  (progn
+    ;; custom graphics that works nice with half-width fringes
+    (fringe-helper-define 'git-gutter-fr+-added nil
+      "..X...."
+      "..X...."
+      "XXXXX.."
+      "..X...."
+      "..X...."
+      )
+    (fringe-helper-define 'git-gutter-fr+-deleted nil
+      "......."
+      "......."
+      "XXXXX.."
+      "......."
+      "......."
+      )
+    (fringe-helper-define 'git-gutter-fr+-modified nil
+      "..X...."
+      ".XXX..."
+      "XX.XX.."
+      ".XXX..."
+      "..X...."
+      )
+    ;; hydra git-gutter
+    (defhydra hydra-git-gutter (:body-pre (global-git-gutter+-mode 1)
+                                          :hint nil)
+      "
+   Git gutter:
+  _j_: next hunk        _s_tage hunk     _q_uit
+  _k_: previous hunk    _r_evert hunk   
+  _m_: git-gutter+-mode _c_ommit 
+  _b_: stage & commit  _B_: stage & commit whole buffer
+"
+      ("j" git-gutter+-next-hunk)
+      ("k" git-gutter+-previous-hunk)
+      ("s" git-gutter+-stage-hunks)
+      ("r" git-gutter+-unstage-whole-buffer)
+      ("m" global-git-gutter+-mode)
+      ("c" git-gutter+-commit)
+      ("b" git-gutter+-stage-and-commit)
+      ("B" git-gutter+-stage-and-commit-whole-buffer)
+      ("q" nil :color blue))
+    ))
 
 
 (provide 'init-git)
