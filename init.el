c8fe74a592a21bf916b6d39b443ffa342320bc68
shyam sunder k <surya46584@gmail.com>
2016-03-06 09:51:38 -0500

    move keybindings

--- init.el
+++ init.el
@@ -3,7 +3,6 @@
 (cask-initialize)
 
 (require 'use-package)
-;; Add /usr/local/bin to path
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
 (setq default-directory (getenv "HOME"))
@@ -70,46 +69,7 @@
 (require 'init-mu4e)
 (require 'init-spaceline)
 (require 'init-workgroups2)
-;;;; Modes ;;;;
-(add-hook 'emacs-lisp-mode-hook 'prettify-symbols-mode)
-(add-hook 'clojure-mode-hook 'prettify-symbols-mode)
-(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
-
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-r") 'helm-recentf)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
-;;(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-;;(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-;;(global-set-key (kbd "M-%") 'query-replace-regexp)
-;;(global-set-key (kbd "C-M-%") 'query-replace)
-;;switch prev nex user buffers
-(global-set-key (kbd "<f11>") 'xah-previous-user-buffer)
-(global-set-key (kbd "<f12>") 'xah-next-user-buffer)
-(global-set-key (kbd "<S-f11>") 'xah-previous-emacs-buffer)
-(global-set-key (kbd "<S-f12>") 'xah-next-emacs-buffer)
-(global-set-key (kbd "<home>") 'xah-backward-left-bracket)
-(global-set-key (kbd "<end>") 'xah-forward-right-bracket)
-(global-set-key (kbd "<prior>") 'xah-backward-block) ; page up key
-(global-set-key (kbd "<next>") 'xah-forward-block) ; page down key
-(global-set-key (kbd "<f8>") 'xah-search-current-word)
-;;(global-set-key (kbd "M-n") 'xah-new-empty-buffer) ; new empty buffer
-(global-set-key (kbd "M-n") 'new-scratch-buffer) ; new empty buffer
-(global-set-key (kbd "<f2>") 'xah-cut-line-or-region) ; cut
-(global-set-key (kbd "<f3>") 'xah-copy-line-or-region) ; copy
-(global-set-key (kbd "<f4>") 'yank) ; paste
-(global-set-key '[(f1)] 'call-last-kbd-macro)
-(global-set-key '[(shift f1)]  'toggle-kbd-macro-recording-on)
-(define-key global-map [remap exchange-point-and-mark] 'exchange-point-and-mark-no-activate)
-(global-set-key (kbd "M-`") 'helm-all-mark-rings)
-(global-set-key (kbd "C-`") 'push-mark-no-activate)
-(global-set-key (kbd "C-[ [ a a") 'push-mark-no-activate)
-(global-set-key [remap mark-sexp] 'easy-mark)
-(global-set-key (kbd "<f7>") 'repeat-complex-command)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key (kbd "C-c I") 'find-user-init-file)
-(global-set-key (kbd "C-c E")  'erase-buffer)
+(require 'init-keybindings)
 
 (let ((local-config (expand-file-name "local.el" user-emacs-directory)))
   (when (file-exists-p local-config)
