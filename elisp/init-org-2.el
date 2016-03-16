c0726ad9b99157b08489a2b524fde3a03eaf00fb
shyam sunder k <surya46584@gmail.com>
2016-03-16 15:32:19 -0400

    update org and evil settings , add org-pomedora

--- elisp/init-org-2.el
+++ elisp/init-org-2.el
@@ -490,12 +490,9 @@
   :commands (org-pomodoro)
   :ensure t
   :config
-  (setq pomodoro-break-time 2)
-  (setq pomodoro-long-break-time 5)
-  (setq pomodoro-work-time 15)
-  ;; (setq-default mode-line-format
-  ;;               (cons '(pomodoro-mode-line-string pomodoro-mode-line-string)
-  ;;                     mode-line-format))
+  (setq org-pomodoro-length 20)
+  (setq org-pomodoro-long-break-length 15)
+  (setq org-pomodoro-short-break-length 5)
   )
 
 (use-package notifications
