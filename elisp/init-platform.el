9045ce1e5946ccfd0ce90774ad8c68597641d790
shyam sunder k <surya46584@gmail.com>
2016-08-02 17:18:43 -0400

    platform,defaults,cask

--- elisp/init-platform.el
+++ elisp/init-platform.el
@@ -10,6 +10,32 @@
  *cygwin* (eq system-type 'cygwin)
  *is-gnu-linux* (eq system-type 'gnu/linux))
 
+(when *is-gnu-linux*
+  (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+  (setq exec-path (append exec-path '("/usr/local/bin")))
+  (setq default-directory (getenv "HOME"))
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives
+               '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+  (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+  (add-to-list 'package-pinned-packages '(ac-cider . "melpa-stable") t)
+  (add-to-list 'package-pinned-packages '(clojure-mode . "melpa-stable") t)
+  (add-to-list 'package-pinned-packages '(clj-refactor . "melpa-stable") t)
+  (add-to-list 'package-pinned-packages '(cider-eval-sexp-fu. "melpa-stable") t)
+
+  ;; keep the installed packages in .emacs.d
+  (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+  ;; Install all packages required
+  (load-file (expand-file-name "elisp/init-elpa-list.el" user-emacs-directory))
+  (package-initialize)
+  ;; Fetch packages the first time
+  (unless (file-exists-p package-user-dir)
+    (package-refresh-contents))
+  (dolist (p my-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
 (when *is-a-mac*
   (setq
    ;; for multilingual environments
@@ -26,15 +52,54 @@
    interprogram-paste-function 'copy-from-osx
    mac-command-modifier nil))
 
-(when *is-gnu-linux*
-  (setq
-   ;; font
-   default-frame-alist '((font . "Monaco-10"))
-   ;; make emacs use the clipboard
-   x-select-enable-clipboard t))
-
 (when *is-a-mac*
   (require 'init-cask)
   )
+
+(defun setup-firacode ()
+  (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
+                 (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
+                 (36 . ".\\(?:>\\)")
+                 (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
+                 (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+                 (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
+                 (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
+                 (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+                 (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
+                 (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+                 (48 . ".\\(?:x[a-zA-Z]\\)")
+                 (58 . ".\\(?:::\\|[:=]\\)")
+                 (59 . ".\\(?:;;\\|;\\)")
+                 (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
+                 (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+                 (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+                 (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
+                 (91 . ".\\(?:]\\)")
+                 (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+                 (94 . ".\\(?:=\\)")
+                 (119 . ".\\(?:ww\\)")
+                 (123 . ".\\(?:-\\)")
+                 (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+                 (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
+                 )
+               ))
+    (dolist (char-regexp alist)
+      (set-char-table-range composition-function-table (car char-regexp)
+                            `([,(cdr char-regexp) 0 font-shape-gstring]))))
+  )
+
+(when *is-gnu-linux*
+  (setq
+   ;; font
+   ;; default-frame-alist '((font . "Monaco-10"))
+   ;; default-frame-alist '((font . "Fira Code-11"))
+   default-frame-alist '((font . "Fira Code-11")
+                         (width . 120)  ;character
+                         (height . 50)) ; lines
+   ;; make emacs use the clipboard
+   x-select-enable-clipboard t)
+  (setup-firacode)
+  )
+
 
 (provide 'init-platform)
