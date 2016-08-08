b8be1cf1f46ca83b7e44726fd1e4da78a7de225d
shyam sunder k <surya46584@gmail.com>
2016-08-08 09:28:50 -0400

    add xah changes

--- elisp/init-keybindings.el
+++ elisp/init-keybindings.el
@@ -8,20 +8,7 @@
 ;;(global-set-key (kbd "M-%") 'query-replace-regexp)
 ;;(global-set-key (kbd "C-M-%") 'query-replace)
 ;;switch prev nex user buffers
-(global-set-key (kbd "<f11>") 'xah-previous-user-buffer)
-(global-set-key (kbd "<f12>") 'xah-next-user-buffer)
-(global-set-key (kbd "<S-f11>") 'xah-previous-emacs-buffer)
-(global-set-key (kbd "<S-f12>") 'xah-next-emacs-buffer)
-(global-set-key (kbd "<home>") 'xah-backward-left-bracket)
-(global-set-key (kbd "<end>") 'xah-forward-right-bracket)
-(global-set-key (kbd "<prior>") 'xah-backward-block) ; page up key
-(global-set-key (kbd "<next>") 'xah-forward-block) ; page down key
-(global-set-key (kbd "<f8>") 'xah-search-current-word)
-;;(global-set-key (kbd "M-n") 'xah-new-empty-buffer) ; new empty buffer
-(global-set-key (kbd "M-n") 'new-scratch-buffer) ; new empty buffer
-(global-set-key (kbd "<f2>") 'xah-cut-line-or-region) ; cut
-(global-set-key (kbd "<f3>") 'xah-copy-line-or-region) ; copy
-(global-set-key (kbd "<f4>") 'yank) ; paste
+
 (global-set-key '[(f1)] 'call-last-kbd-macro)
 (global-set-key '[(shift f1)]  'toggle-kbd-macro-recording-on)
 (define-key global-map [remap exchange-point-and-mark] 'exchange-point-and-mark-no-activate)
