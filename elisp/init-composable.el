55a1203e7daf198a29e801bcd15bdf0ca47eb1e3
shyam sunder k <surya46584@gmail.com>
2016-08-26 15:32:17 -0400

    composable

---
+++ elisp/init-composable.el
@@ -0,0 +1,20 @@
+(use-package composable
+  :diminish composable-mode
+  :ensure t
+  :config
+  (progn
+    (unbind-key (kbd "p") composable-object-mode-map)
+    (unbind-key (kbd "n") composable-object-mode-map)
+    (bind-keys :map composable-object-mode-map
+               ((kbd "j") . next-line)
+               ((kbd "d") . er/mark-defun)
+               ((kbd "k") . previous-line))
+
+    (composable-def '(delete-region))
+    (composable-def '(lispy-new-copy))
+    (composable-def '(lispy-yank))
+
+    (composable-mode)
+    (composable-mark-mode)))
+
+(provide 'init-composable)
