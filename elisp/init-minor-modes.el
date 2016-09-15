9e759095e959cd0814a6007af83a3dcf1ac9aa0b
shyam sunder k <surya46584@gmail.com>
2016-09-15 17:38:08 -0400

    view mode

--- elisp/init-minor-modes.el
+++ elisp/init-minor-modes.el
@@ -171,4 +171,40 @@
     (my/turn-on-hl-line-mode)
     (call-interactively #'helm-occur)))
 
+(use-package view
+  :defer t
+  :bind
+  (("C-M-n" . View-scroll-half-page-forward)
+   ("C-M-p" . View-scroll-half-page-backward))
+  :config
+  (progn
+    ;; When in view-mode, the buffer is read-only:
+    (setq view-read-only t)
+
+    (defun View-goto-line-last (&optional line)
+      "goto last line"
+      (interactive "P")
+      (goto-line (line-number-at-pos (point-max))))
+
+    ;; less like
+    (define-key view-mode-map (kbd "N") 'View-search-last-regexp-backward)
+    (define-key view-mode-map (kbd "?") 'View-search-regexp-backward?)
+    (define-key view-mode-map (kbd "g") 'View-goto-line)
+    (define-key view-mode-map (kbd "G") 'View-goto-line-last)
+    ;; vi/w3m like
+    (define-key view-mode-map (kbd "h") 'backward-char)
+    (define-key view-mode-map (kbd "j") 'next-line)
+    (define-key view-mode-map (kbd "k") 'previous-line)
+    (define-key view-mode-map (kbd "l") 'forward-char)))
+
+(use-package doc-view
+  :config
+  (define-key doc-view-mode-map (kbd "j")
+    #'doc-view-next-line-or-next-page)
+  (define-key doc-view-mode-map (kbd "k")
+    #'doc-view-previous-line-or-previous-page)
+  ;; use 'q' to kill the buffer, not just hide it
+  (define-key doc-view-mode-map (kbd "q")
+    #'kill-this-buffer))
+
 (provide 'init-minor-modes)
